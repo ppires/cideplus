027e38fc2f7580da72a66da4404ac74781fc2514
Pedro Pires <pedro.pires@dito.com.br>
2012-02-09 11:57:38 -0200

    dynamic menu with parameters

--- src/cideplus/commands/handlers/MarkFeatureHandler.java
+++ src/cideplus/commands/handlers/MarkFeatureHandler.java
@@ -4,17 +4,36 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.ITextSelection;
+
+import cideplus.ui.presentation.markers.FeaturesMarkerFactory;
+import cideplus.utils.PluginUtils;
 
 public class MarkFeatureHandler extends AbstractHandler implements IHandler {
+
+	private String featureParameterId = "cideplus.commands.markFeature.featureIdParameter";
 
 	public MarkFeatureHandler() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		MessageDialog.openInformation(HandlerUtil.getActiveShellChecked(event), "My Handler", "Not yet implemented");
+
+		// Working!
+		int featureId = Integer.parseInt(event.getParameter(featureParameterId));
+
+		// Needs tests... Copied from FeaturesMarkerFactory.
+		ITextSelection selection = PluginUtils.getCurrentTextSelection();
+		try {
+			IResource resource = PluginUtils.getCurrentFile();
+			FeaturesMarkerFactory.createMarker(resource, selection, featureId);
+		} catch (CoreException e) {
+			System.out.println("Caught Exception creating marker!!!");
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 
--- src/cideplus/ui/editor/popup/actions/MenuContentProvider.java
+++ src/cideplus/ui/editor/popup/actions/MenuContentProvider.java
@@ -1,15 +1,21 @@
 package cideplus.ui.editor.popup.actions;
 
+import java.util.HashMap;
+
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.swt.SWT;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.CompoundContributionItem;
 import org.eclipse.ui.menus.CommandContributionItem;
 import org.eclipse.ui.menus.CommandContributionItemParameter;
+import org.eclipse.ui.services.IServiceLocator;
 
 public class MenuContentProvider extends CompoundContributionItem {
 
 	private static int counter = 0;
+
+	//	private static Set<Feature> features;
+	private static String featureIdParameter = "cideplus.commands.markFeature.featureIdParameter";
 
 	public MenuContentProvider() {
 		// TODO Auto-generated constructor stub
@@ -20,14 +26,22 @@ public class MenuContentProvider extends CompoundContributionItem {
 		// TODO Auto-generated constructor stub
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected IContributionItem[] getContributionItems() {
+		IServiceLocator serviceLocator = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		String id = "cideplus.commands.markFeature.commandParameterId";
+		String commandId = "cideplus.commands.markFeature";
+
 		final CommandContributionItemParameter contributionParameter =
-				new CommandContributionItemParameter(
-						PlatformUI.getWorkbench().getActiveWorkbenchWindow(),
-						"my.project.myCommandContributionItem", "cideplus.commands.markFeature",
-						SWT.NONE);
+				new CommandContributionItemParameter(serviceLocator, id, commandId, SWT.NONE);
 		contributionParameter.label = "Dynamic Menu Item " + counter++;
+
+		Object key = "cideplus.commands.markFeature.featureIdParameter";
+		Object value = "123";
+		contributionParameter.parameters = new HashMap();
+		contributionParameter.parameters.put(key, value);
+
 		return new IContributionItem[] {
 				new CommandContributionItem(contributionParameter) };
 	}
